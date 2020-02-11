1089836227cf4cce7857dfcef4ba2bd68d5e8efb
torrence <tlstratton@gmail.com>
2020-02-11 10:02:56 -0800

    Add methods

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/mostfamiliar/shippy-cli-consignment
 
 go 1.13
+
+require (
+	github.com/mostfamiliar/shippy-service-consignment v0.0.0-20200211164533-8e59bea42a74
+	google.golang.org/genproto v0.0.0-20200211111953-2dc5924e3898 // indirect
+	google.golang.org/grpc v1.27.1
+)
--- main.go
+++ main.go
@@ -2,12 +2,11 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"os"
-
-	"context"
 
 	pb "github.com/mostfamiliar/shippy-service-consignment/proto/consignment"
 	"google.golang.org/grpc"
@@ -54,4 +53,12 @@ func main() {
 		log.Fatalf("Could not greet: %v", err)
 	}
 	log.Printf("Created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
 }
